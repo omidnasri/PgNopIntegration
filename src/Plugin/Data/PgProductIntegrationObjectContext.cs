60ff9515465f1f74c503342f765dfc756e94c91c
mohammad taghipoor <mohammad taghipoor>
2016-07-07 19:17:53 +0430

    added PgProductMetaData Entity

---
+++ src/Plugin/Data/PgProductIntegrationObjectContext.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Nop.Core;
+using Nop.Data;
+
+namespace Septa.PgNopIntegration.Plugin.Data
+{
+    public class PgProductIntegrationObjectContext : DbContext, IDbContext
+    {
+        public PgProductIntegrationObjectContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        #region Implementation of IDbContext
+
+        #endregion
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new PgProductMetaDataMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public string CreateDatabaseInstallationScript()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.CreateDatabaseScript();
+        }
+
+        public void Install()
+        {
+            //It's required to set initializer to null (for SQL Server Compact).
+            //otherwise, you'll get something like "The model backing the 'your context name' context has changed since the database was created. Consider using Code First Migrations to update the database"
+            Database.SetInitializer<PgProductIntegrationObjectContext>(null);
+
+            Database.ExecuteSqlCommand(CreateDatabaseInstallationScript());
+            SaveChanges();
+        }
+
+        public void Uninstall()
+        {
+            var dbScript = "DROP TABLE PgProductMetaData";
+            Database.ExecuteSqlCommand(dbScript);
+            SaveChanges();
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
+        {
+            return base.Set<TEntity>();
+        }
+
+        public System.Collections.Generic.IList<TEntity> ExecuteStoredProcedureList<TEntity>(string commandText, params object[] parameters) where TEntity : BaseEntity, new()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IEnumerable<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ExecuteSqlCommand(string sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Detach(object entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
+
+            ((IObjectContextAdapter)this).ObjectContext.Detach(entity);
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether proxy creation setting is enabled (used in EF)
+        /// </summary>
+        public virtual bool ProxyCreationEnabled
+        {
+            get
+            {
+                return this.Configuration.ProxyCreationEnabled;
+            }
+            set
+            {
+                this.Configuration.ProxyCreationEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether auto detect changes setting is enabled (used in EF)
+        /// </summary>
+        public virtual bool AutoDetectChangesEnabled
+        {
+            get
+            {
+                return this.Configuration.AutoDetectChangesEnabled;
+            }
+            set
+            {
+                this.Configuration.AutoDetectChangesEnabled = value;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Plugin/PgProductIntegrationPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Nop.Core.Plugins;
+using Septa.PgNopIntegration.Plugin.Data;
+using Septa.PgNopIntegration.Plugin.Domain;
+using Nop.Core.Data;
+
+namespace Septa.PgNopIntegration.Plugin
+{
+    public class PgProductIntegrationPlugin : BasePlugin
+    {
+        private readonly PgProductIntegrationObjectContext _pgProductIntegrationContext;
+        private readonly IRepository<PgProductMetaData> _pgProductMetaDataRepository;
+
+        public PgProductIntegrationPlugin(PgProductIntegrationObjectContext pgProductIntegrationContext, IRepository<PgProductMetaData> pgProductMetaDataRepository)
+        {
+            this._pgProductIntegrationContext = pgProductIntegrationContext;
+            this._pgProductMetaDataRepository = pgProductMetaDataRepository;
+        }
+
+        public override void Install()
+        {
+            _pgProductIntegrationContext.Install();
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            _pgProductIntegrationContext.Uninstall();
+            base.Uninstall();
+        }
+    }
+}
