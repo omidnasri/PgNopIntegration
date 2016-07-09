d2f08418a498db890ff1e064b4fc9dd7259e0663
b.esmailloo <b.esmailloo@gmail.com>
2016-07-10 00:33:56 +0430

    added payam gostar product services, custom dependency registrar + unit tests

---
+++ src/Plugin.Tests/PayamGostarPluginDependencyRegistrarTests.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Moq;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Septa.PayamGostar.ServiceLayer.ServiceType.Api;
+using Septa.PgNopIntegration.Plugin.PayamGostarService;
+using Xunit;
+
+namespace Septa.PgNopIntegration.Plugin.Tests
+{
+    public class PayamGostarPluginDependencyRegistrarTests
+    {
+        [Fact]
+        public void RegistrarOrderIsZero()
+        {
+            var registrar = new PayamGostarPluginDependencyRegistrar();
+
+            Assert.Equal(0, registrar.Order);
+        }
+
+        [Fact]
+        public void IProductSyncServiceIsRegisteredInBuilder()
+        {
+            var containerBuilder = new ContainerBuilder();
+            var typeFinder = Mock.Of<ITypeFinder>();
+            var nopConfig = Mock.Of<NopConfig>();
+            var registrar = new PayamGostarPluginDependencyRegistrar();
+
+            registrar.Register(containerBuilder, typeFinder, nopConfig);
+            var container = containerBuilder.Build(Autofac.Builder.ContainerBuildOptions.None);
+            
+            Assert.IsType<ProductSyncService>(container.Resolve<IProductSyncService>());
+        }
+    }
+}
---
+++ src/Plugin/PayamGostarService/ProductChanges.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Septa.PayamGostar.ServiceLayer.Contract.DataContracts
+{
+    /// <summary>
+    /// Represents changes in Payam Gostar products.
+    /// Stores deleted, edited and new products.
+    /// </summary>
+    [DataContract]
+    public class ProductChanges
+    {
+        /// <summary>
+        /// Product codes that are no longer in Payam Gostar
+        /// </summary>
+        public IEnumerable<string> DeletedProductCodes  { get; set; }
+
+        /// <summary>
+        /// Products that are edited since last sync
+        /// </summary>
+        public IEnumerable<Product> EditedProducts { get; set; }
+
+        /// <summary>
+        /// Products that are inserted since last sync
+        /// </summary>
+        public IEnumerable<Product> NewProducts { get; set; }
+    }
+}
